ff83bfd5e80911dbf572b035c4fe36953e5a0716
diasvictordev <victor.dias@aluno.ueg.br>
2023-09-15 20:07:18 -0300

    first commit

---
+++ src/main/java/com/cartaofidelidade/cartaofidelidade/CartaofidelidadeApplication.java
@@ -0,0 +1,13 @@
+package com.cartaofidelidade.cartaofidelidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaofidelidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaofidelidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cartaofidelidade/cartaofidelidade/CartaofidelidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cartaofidelidade.cartaofidelidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartaofidelidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
